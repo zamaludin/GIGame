e80bf4e77f342016d89097d5125041c2ecf8585e
zamaludin abdulah <zamaludin@live.com>
2023-03-27 13:40:54 +0700

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GIGame",
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -14,15 +15,31 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+      .package(path: "../GIAPI"),
+      .package(path: "../GICommon"),
+      .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
+      .package(url: "https://github.com/WeTransfer/Mocker.git", .upToNextMajor(from: "3.0.1")),
+      
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GIGame",
-            dependencies: []),
+            dependencies: [
+                "GIAPI",
+                "GICommon",
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "Mocker", package: "Mocker")
+            ]
+        ),
         .testTarget(
             name: "GIGameTests",
-            dependencies: ["GIGame"]),
+            dependencies: ["GIGame"],
+            resources: [
+              .process("games.json"),
+              .process("gamedetail.json")
+            ]
+        )
     ]
 )
